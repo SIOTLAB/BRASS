f3e7c2147ef120a635fbbb4b74ea013ad85a5a82
ConcreteBridge <conradspark@gmail.com>
2022-01-20 10:28:00 -0800

    Advertise the controller's IP address

--- modules/tcp/server.py
+++ modules/tcp/server.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
 
-import socket
+#  import socket
 
-HOST = '127.0.1.1'
-PORT = 65432
+#  HOST = '127.0.1.1'
+#  PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+#  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#      s.bind((HOST, PORT))
+#      s.listen()
+#      conn, addr = s.accept()
+#      with conn:
+#          print('Connected by', addr)
+#          while True:
+#              data = conn.recv(1024)
+#              if not data:
+#                  break
+#              conn.sendall(data)
+
+from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST, \
+    gethostbyname_ex, gethostname
+from time import sleep
+
+PORT = 50000
+MAGIC = "Jts45F2t8cMgrNT2"
+
+s = socket(AF_INET, SOCK_DGRAM)  # create UDP socket
+s.bind(('', 0))
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  # this is a broadcast socket
+my_ip = gethostbyname_ex(gethostname())
+
+while 1:
+    data = MAGIC + str(my_ip)
+    s.sendto(data.encode(), ('<broadcast>', PORT))
+    print('sent service announcement')
+    sleep(2)
