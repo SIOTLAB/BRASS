2b7fa8f44e63fb71fc463415e396bda9d67089cb
ConcreteBridge <conradspark@gmail.com>
2022-01-27 10:27:01 -0800

    It finally works!

--- modules/udp/discover/launch.sh
+++ modules/udp/discover/launch.sh
@@ -1,9 +1,7 @@
-#!/bin/dash
-
 if [ "$1" = "client" ]; then
-	python3 discover_client.py
+	python discover_client.py
 elif [ "$1" = "server" ]; then
-	python3 discover_server.py & echo $! > discovery_server.pid
+	python discover_server.py & echo $! > discovery_server.pid
 else
 	echo "$0" "<client/server>"
 fi
